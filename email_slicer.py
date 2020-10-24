6e7908df96fc7ad4618e738366a501c7942e5399
wasi-master <63045920+wasi-master@users.noreply.github.com>
2020-10-24 15:32:25 +0600

    Update email_slicer.py

--- email_slicer.py
+++ email_slicer.py
@@ -1,8 +1,11 @@
-email  = input("Enter a email:\n")
+email  = input("Enter a email:\n") # We take a email
 
-parts = email.split("@")
-user, domain = parts
+parts = email.split("@") # We split the email by @ and get a list and save it as parts
+user, domain = parts # We save to parts to 2 variables
+# Same as:
+# user   = parts[0]
+# domain = parts[1]
 
-print("\n")
-print(f"Username: {user}")
-print(f"Domain: {domain}")
+print("\n") # We print a newline to add separation
+print(f"Username : {user}") # We print the username
+print(f"Domain   : {domain}") # We print the domain name
